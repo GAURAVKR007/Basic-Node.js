b61c2f810db1d06f4a4bec8002768915cf32f325
GAURAVKR007 <gauravkrthakur07@gmail.com>
2022-07-28 11:54:38 +0530

    Supervillan package added using npm packages

--- Node-Basics/index.js
+++ Node-Basics/index.js
@@ -3,7 +3,11 @@
 
 // fs.copyFileSync("file1.txt","file2.txt");
 
-const superheroes = require('superheroes');
+console.log("==========================================");
+
+console.log("Superheroes");
+
+const superheroes = require('superheroes : ');
 var hero = [];
 
 for(var i = 1;i<=10;i++){
@@ -12,3 +16,17 @@ hero.push(superheroes.random());
 
 console.log(hero);
 // console.log(superheroes.all);
+
+console.log("==========================================");
+
+console.log("Supervillans : ");
+
+const supervillains = require('supervillains');
+
+var villan = [];
+
+for(var i = 1;i<=10;i++){
+villan.push(superheroes.random());
+}
+
+console.log(villan);
