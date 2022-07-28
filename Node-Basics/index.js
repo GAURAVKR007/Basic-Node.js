a3667631339b9a8685b3d496aaba2a609e7b9879
GAURAVKR007 <gauravkrthakur07@gmail.com>
2022-07-28 11:46:29 +0530

    Installing dependencies in our package.json from npm packages

--- Node-Basics/index.js
+++ Node-Basics/index.js
@@ -1,4 +1,14 @@
 
-const fs = require('fs');
+// const fs = require('fs');
 
-fs.copyFileSync("file1.txt","file2.txt");
+// fs.copyFileSync("file1.txt","file2.txt");
+
+const superheroes = require('superheroes');
+var hero = [];
+
+for(var i = 1;i<=10;i++){
+hero.push(superheroes.random());
+}
+
+console.log(hero);
+// console.log(superheroes.all);
